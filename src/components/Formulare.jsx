807e64d42b8b401563b9c2208fdcc3d116da61a2
Erizomovil <jg.arcalagonzalez@gmail.com>
2025-04-24 16:06:12 +0100

    fixes3

--- src/components/Formulare.jsx
+++ src/components/Formulare.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './formulare.css';
+import './Formulare.css';
 
 const Formulare = ({ setJsonData }) => {
     const [formData, setFormData] = useState({
